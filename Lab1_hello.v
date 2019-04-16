817dc0d09ea2eaf550a53d50f910f8209b64c3b7
Wasif Fahim <wasif.fahim@gmail.com>
2019-04-15 17:57:50 -0700

    finalized

--- Lab1_hello.v
+++ Lab1_hello.v
@@ -6,17 +6,17 @@
 //   Bryan Chin
 //   Lih-Feng Tsaur
 //
-// Author:
+// Author: Wasif Fahim
 //
 
-module Lab1_hello(input tb_a, input tb_b, input tb_c, output L1_andOut, output L1_orOut, output L1_nandOut, output L1_norOut, output L1_notOut_a);
+module Lab1_hello(input tb_a, input tb_b, input tb_c, output L1_andOut, output L1_orOut, output L1_nandOut, output L1_norOut, output L1_notOut_a); //Function that handles all inputs and ouputs
 
    //Example boolean operation
-   assign L1_andOut = tb_a & tb_b & tb_c;
-	assign L1_orOut = tb_a | tb_b | tb_c;
-	assign L1_nandOut = ~(tb_a & tb_b & tb_c);
-	assign L1_norOut = ~(tb_a | tb_b | tb_c);
-	assign L1_notOut_a = ~tb_a;
+   assign L1_andOut = tb_a & tb_b & tb_c;       //AND statement
+	assign L1_orOut = tb_a | tb_b | tb_c;        //OR statement  
+	assign L1_nandOut = ~(tb_a & tb_b & tb_c);   //NOT AND statement
+	assign L1_norOut = ~(tb_a | tb_b | tb_c);    //NOT OR statement
+	assign L1_notOut_a = ~tb_a;                  //NOT statement 
    
 
-endmodule
+endmodule   //End of function
--- tb_hello.v
+++ tb_hello.v
@@ -7,7 +7,7 @@
 //   Bryan Chin
 //   Lih-Feng Tsaur
 //
-// Author:
+// Author: Wasif Fahim
 //
 
 `timescale 1ns/1ns
@@ -16,43 +16,43 @@
 // Lab1 testbench
 // runs some basic tests on Lab1_hello
 //
-module tb_hello;
+module tb_hello;   //The hello function or method
 
-   wire L1_andOut;
-   reg 	tb_a;
+   wire L1_andOut;  //Output wire
+   reg 	tb_a;      //All the variables getting declared
    reg 	tb_b;
    reg  tb_c;
    
    initial begin
-      tb_a <= 0;
-      tb_b <= 0;
-      tb_c <= 0;
-      #20;
-      tb_a <= 0;
-      tb_b <= 0;
-      tb_c <= 1;
+      tb_a <= 0;  //Initalizing value to 0
+      tb_b <= 0;  //Initalizing value to 0
+      tb_c <= 0;  //Initalizing value to 0
+      #20;        //20 nanosecond delay given before the next step
+      tb_a <= 0;  //Initalizing value to 0
+      tb_b <= 0;  //Initalizing value to 0
+      tb_c <= 1;  //Initializing value to 1
       #20;
       $finish;
    end
 
    initial begin
       //#20;
-      $display("Hello There Wasif Fahim");
+      $display("Hello There Wasif Fahim");   //Displays Hello statement
    end
 
    initial begin
-		#20;
-      $display("GoodBye!!!");
-   end
+		#20;        //20 nanosecond delay given so Goodbye could display after hello
+      $display("GoodBye!!!");   //Displays Goodbye
+   end    
    
    
-   Lab1_hello Lab1_hello(.tb_a(tb_a), .tb_b(tb_b), .tb_c(tb_c), .L1_andOut(L1_andOut));
+   Lab1_hello Lab1_hello(.tb_a(tb_a), .tb_b(tb_b), .tb_c(tb_c), .L1_andOut(L1_andOut));  //Test Bench
 	//Lab1_hello2 Lab1_hello(.tb_a(tb_a), .tb_b(tb_b), .tb_c(tb_c), .L1_orOut(L1_orOut));
 	//Lab1_hello3 Lab1_hello(.tb_a(tb_a), .tb_b(tb_b), .tb_c(tb_c), .L1_nandOut(L1_nandOut));
 	//Lab1_hello4 Lab1_hello(.tb_a(tb_a), .tb_b(tb_b), .tb_c(tb_c), .L1_norOut(L1_norOut));
 	//Lab1_hello5 Lab1_hello(.tb_a(tb_a), .L1_notOut(L1_notOut));
 
-endmodule
+endmodule   //Ends the function
       
       
       
